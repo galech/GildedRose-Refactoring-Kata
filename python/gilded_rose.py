b7796293073c64993ac6ce811c87ee89051f8cd3
agalech <alex_galech@hotmail.com>
2024-12-30 19:56:01 +0100

    New update_quality algo && Cursed added

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,42 +1,87 @@
 # -*- coding: utf-8 -*-
 
+
+MAX_QUALITY = 50  # except for legendary items
+
+
+class NewItem:
+
+    legendary = False
+    quality_change_by_day = -1  # base quality change by day
+    custom_intervals = None
+
+    def __init__(self, base_item):
+
+        self.base_item = base_item
+        self.legendary = base_item.name == "Sulfuras, Hand of Ragnaros"
+
+        if base_item.name == "Aged Brie":
+            self.quality_change_by_day *= -1  # inverse the logic ;)
+
+        if base_item.name.startswith("Conjured"):
+            self.quality_change_by_day *= 2  
+
+        if base_item.name.startswith("Backstage passes"):
+            self.custom_intervals = [
+                {"lte": -1, "change": -MAX_QUALITY},  # need quality to be fixed
+                {"interval": [0, 5], "change": 3},
+                {"interval": [6, 10], "change": 2},
+                {"gte": 11, "change": 1}
+            ]
+
+        self.fix_quality()
+
+    def fix_quality(self):
+
+        if not self.legendary:  
+
+            self.base_item.quality = max(0, min(MAX_QUALITY, self.base_item.quality))
+
+    @property
+    def sell_in(self):
+
+        return self.base_item.sell_in
+
+    @property
+    def compute_quality_delta(self):
+
+        if self.custom_intervals:  # custom case
+            for i_dict in self.custom_intervals:
+                if "lte" in i_dict and self.sell_in <= i_dict["lte"]:
+                    return i_dict["change"]
+                if "gte" in i_dict and self.sell_in >= i_dict["gte"]:
+                    return i_dict["change"]
+                if "interval" in i_dict and (i_dict["interval"][0] <= self.sell_in <= i_dict["interval"][1]):
+                    return i_dict["change"]
+
+            return 1
+
+        else:  # normal case
+            return self.quality_change_by_day * (2 if self.sell_in < 0 else 1)
+
+    def handle_new_day(self):
+
+        if not self.legendary:
+        
+            self.base_item.sell_in -= 1
+            self.base_item.quality += self.compute_quality_delta
+            self.fix_quality()
+
+
 class GildedRose(object):
 
     def __init__(self, items):
-        self.items = items
+
+        self.items = [NewItem(item) for item in items]
 
     def update_quality(self):
+
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            item.handle_new_day()
 
 
 class Item:
+
     def __init__(self, name, sell_in, quality):
         self.name = name
         self.sell_in = sell_in
